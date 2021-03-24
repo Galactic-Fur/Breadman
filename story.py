984e5e46704983de4d749666d3673f92909c0caa
god-of-git <80875024+god-of-git@users.noreply.github.com>
2021-03-24 01:28:54 -0700

    Update story.py

--- story.py
+++ story.py
@@ -4,63 +4,67 @@ from faces import story_faces as sf
 line_1 = "Hi! I am Breadman."
 line_2 = "I have been accused for stealing bread."
 line_3 = "But, I am innocent."
-line_4 = "They say, i will not be hanged till death if i play a game called Hangman."
+line_4 = "They say, I will not be hanged till death if I play a game called Hangman."
 line_5 = "Now, you, and only you, can save me, your Breadman, from getting hanged."
 
 def story():
-	print()
-	for x in line_1:
-		print(x, end = "")
-		time.sleep(0.1)
-	print()
-	print(sf[1])
+    print()
+    for x in line_1:
+        print(x, end = "")
+        time.sleep(0.1)
+    print()
+    print(sf[1])
 
-	input("Press Enter to continue.")
+    input("Press Enter to continue.")
 
-	print()
-	for x in line_2:
-		print(x, end = "")
-		time.sleep(0.1)
-	print()
-	time.sleep(1)
-	for x in line_3:
-		print(x, end = "")
-		time.sleep(0.1)
-	print()
-	print(sf[2])
+    print()
+    for x in line_2:
+        print(x, end = "")
+        time.sleep(0.1)
+    print()
+    time.sleep(1)
+    for x in line_3:
+        print(x, end = "")
+        time.sleep(0.1)
+    print()
+    print(sf[2])
 
-	input("Press Enter to continue.")
+    input("Press Enter to continue.")
 
-	print()
-	for x in line_4:
-		print(x, end = "")
-		time.sleep(0.1)
-	print()
-	time.sleep(1)
-	for x in line_5:
-		print(x, end = "")
-		time.sleep(0.1)
-	print()
-	print(sf[2])
+    print()
+    for x in line_4:
+        print(x, end = "")
+        time.sleep(0.1)
+    print()
+    time.sleep(1)
+    for x in line_5:
+        print(x, end = "")
+        time.sleep(0.1)
+    print()
+    print(sf[2])
 
-	input("Press Enter to continue.")
+    input("Press Enter to continue.")
 
-def rules_or_start():
-	print("\n1: Start\n2: Rules")
 
-	lets_begin = input("\nSelect your option: ")
-	if int(lets_begin) == 2:
-		print("\nRandomly, a word will be printed on your screen, in which only one letter will be shown to you that's in that word.")
-		time.sleep(5)
-		print("You'll have 5 attempts to guess the word correctly.")
-		time.sleep(5)
-		print("If all your attempts are over and you don't guess the word correctly, Breadman dies.")
-		time.sleep(5)
-		print("Good luck!")
-		time.sleep(1)
-		rules_or_start()
-	elif int(lets_begin) != 1:
-		print("\nInvalid option.")
-		rules_or_start()
-	else:
-		pass
+def now_the_game():
+    print("\n1: Start\n2: Rules")
+
+    rules_or_start = int(input("\nChoose your option: "))
+
+    if rules_or_start == 2:
+        print()
+        print("Randomly, a word will be printed on the screen, with only one letter of it shown to you.")
+        time.sleep(5)
+        print("You will have to guess what might be the letters in that word, one by one.")
+        time.sleep(5)
+        print("You have 5 attempts until Breadman dies.")
+        time.sleep(3)
+        print("Good luck!")
+        time.sleep(2)
+        input("\nPress Enter to go back to the two-option menu.")
+        now_the_game()
+    elif rules_or_start != 1:
+        print("\nInvalid option.")
+        now_the_game()
+    else:
+        pass
